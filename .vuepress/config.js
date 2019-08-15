3b1b64dfa69b8905b31f9be0a53f35e046f91f1d
DengYunHua <heihei_yh@163.com>
2019-08-15 09:30:30 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  base: '/docs/',
+  title: 'Wintersweet',
+  description: 'ECMAScript 6.0 教程',
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'ECMAScript 6', link: '/es6/' },
+      { text: 'JavaFX', link: '/javafx/' },
+      { text: 'Vue', link: '/vue/' }
+    ]
+  }
+}
